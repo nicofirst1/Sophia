4befd5a0438ce57bf9040408949611ea335e422a
dizzi <nicofirst1@gmail.com>
2021-12-23 15:27:16 +0100

    add dynamic questions

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+dataset_file_path = "/home/dizzi/Downloads/Dataset.xlsx"
+
 
 def read_data(path2file):
     """
@@ -39,6 +41,46 @@ def read_data(path2file):
 
     df = pd.concat([x_data, y_data], axis=1)
     return df
+
+
+def expand_all_key2(dataframe):
+    """
+    Some of the questions have multiple answers, such answers have also 'all' as a possibility.
+    Exapand all to all possibilities
+    :param dataframe:
+    :return:
+    """
+    for all_coll in dataframe.columns.values:
+        if "all" not in all_coll:
+            continue
+
+        # get parent name
+        parent_label = all_coll.strip(":all")
+
+        # get rows where all_coll is 1
+        all_rows = dataframe[dataframe[all_coll] == 1]
+
+        # drop all_coll
+        all_rows = all_rows.drop(all_coll, axis=1)
+
+        new_rows = []
+        for idx, row in all_rows.iterrows():
+
+            row_indices = [elem for elem in row.index.values if parent_label in elem]
+
+            for r_idx in row_indices:
+                n_row = row.copy()
+                n_row[r_idx] = 1
+                new_rows.append(n_row)
+
+        # drop indices where all=1
+        dataframe = dataframe.drop(all_rows.index)
+        dataframe = dataframe.drop(all_coll, axis=1)
+
+        new_rows = pd.concat(new_rows, axis=1).T
+        dataframe = pd.concat([dataframe, new_rows], ignore_index=True)
+
+    return dataframe
 
 
 def expand_all_key(dataframe):
@@ -81,7 +123,44 @@ def expand_all_key(dataframe):
     return dataframe
 
 
-def data_pipeline(path2file, classification=False):
+def categoriacl_data(df):
+    """
+    Transform from one hot encoder to category
+    :param df:
+    :return:
+    """
+    labels = [x for x in df.columns.values if ":" in x]
+    labels = [x for x in labels if "all" not in x]
+
+    parents_l = [x.split(":")[0] for x in labels]
+    parents_l = list(set(parents_l))
+
+    label_dict = dict()
+    for p in parents_l:
+        label_dict[p] = {}
+
+        lbs = [x for x in labels if p in x]
+        idx = 1
+        for l in lbs:
+            label_dict[p][l] = idx
+            idx += 1
+
+    for p in parents_l:
+
+        lbs = [x for x in labels if p in x and "all" not in x]
+        p_df = df[lbs]
+        for l in lbs:
+            p_df[p_df > 0] = 1
+            p_df[l].replace({1: label_dict[p][l]}, inplace=True)
+
+        p_df = p_df.sum(axis=1)
+        df = df.drop(lbs, axis=1)
+        df[p] = p_df
+
+    return df.drop_duplicates()
+
+
+def data_pipeline(path2file):
     """
     Run the entire pipeline and return x/y data
     :param path2file:
@@ -90,6 +169,8 @@ def data_pipeline(path2file, classification=False):
     df = read_data(path2file)
     df = expand_all_key(df)
 
+    # df = categoriacl_data(df)
+
     label_indices = [x for x in df.columns if "Label" in x]
 
     y_data = df[label_indices]
@@ -97,21 +178,27 @@ def data_pipeline(path2file, classification=False):
     y_labels = [x.strip("Labels") for x in y_data.columns]
     y_labels = [x.strip(":") for x in y_labels]
 
-    if classification:
-        y_data[y_data != 0] = 1
-        label_encoding = {y_data.columns[idx]: idx for idx in range(len(y_data.columns))}
-        y_data["Labels"] = -1
-        for k, v in label_encoding.items():
-            y_data["Labels"][y_data[k] == 1] = v
+    y_data[y_data != 0] = 1
+    label_encoding = {y_data.columns[idx]: idx for idx in range(len(y_data.columns))}
+    y_data["Labels"] = -1
+    for k, v in label_encoding.items():
+        y_data["Labels"][y_data[k] == 1] = v
 
-        y_data = y_data["Labels"]
+    y_data = y_data["Labels"]
 
     return x_data, y_data, y_labels
 
 
-if __name__ == '__main__':
-    path2file = "/home/dizzi/Downloads/Dataset.xlsx"
+def get_labels(path2file):
+    x_data, _, y_labels = data_pipeline(path2file)
+    x_labels = x_data.columns.values
 
-    x_df, y_df = data_pipeline(path2file)
+    x_labels = x_labels.tolist()
+
+    return x_labels + y_labels
+
+
+if __name__ == '__main__':
+    x_df, y_df = data_pipeline(dataset_file_path)
 
     print(x_df)
--- decision_tree.py
+++ decision_tree.py
@@ -1,29 +1,78 @@
+import random
+
 import graphviz as graphviz
+import numpy as np
 from sklearn import tree
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
 
 from data_processing import data_pipeline
 
+
+def print_question(label):
+    try:
+        cat, answ = label.split(":")
+        return f"{cat} -> {answ}?[y/n] "
+    except ValueError:
+        return f"{label}?[y/n] "
+
+
+def print_prob(probs, y_label):
+    for prob, label in zip(probs, y_label):
+        print(f"\t{label} = {prob * 100:.2f}%")
+    print("\n\n")
+
+
+def interactive_questions(classifier, feat_lab, target_lab):
+    tree = classifier.tree_
+    node = 0  # Index of root node
+    idx = random.choice(range(len(feat_lab)))
+    point = np.zeros((1, len(feat_lab)))
+
+    while True:
+        feat, thres = tree.feature[node], tree.threshold[node]
+        # print(feat_lab[feat], thres)
+        v = input(print_question(feat_lab[feat]))
+        if v == "n":
+            node = tree.children_left[node]
+
+        else:
+            node = tree.children_right[node]
+            point[0][feat] = 1
+
+        prob = classifier.predict_proba(point)[0]
+        print_prob(prob, y_label=y_labels)
+        if tree.children_left[node] == tree.children_right[node]:  # Check for leaf
+            label = np.argmax(tree.value[node])
+            print(f"Predicted Label is: {target_lab[label]}")
+            print(f"Gini : {tree.impurity[node]} (close to zero is betterr)")
+            break
+
+
 if __name__ == '__main__':
     path2file = "/home/dizzi/Downloads/Dataset.xlsx"
-    classification = True
     random_state = 42
 
-    X, y, y_labels = data_pipeline(path2file, classification=classification)
+    X, y, y_labels = data_pipeline(path2file)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
     x_labels = X_test.columns.values
 
-    attributes = dict(
-        random_state=random_state
-    )
+    class_weight = {}
 
-    if classification:
-        decision_tree = DecisionTreeClassifier(criterion="entropy", **attributes)
-    else:
-        decision_tree = DecisionTreeRegressor(criterion="squared_error", **attributes)
+    for idx, yl in enumerate(y_labels):
+        if "Brest" in yl:
+            class_weight[idx] = 0.146
+        elif "Colon" in yl:
+            class_weight[idx] = 0.116
+        elif "Lung" in yl:
+            class_weight[idx] = 0.109
+        elif "Prostate" in yl:
+            class_weight[idx] = 0.096
+
+    decision_tree = DecisionTreeClassifier(criterion="entropy", random_state=random_state,
+                                           max_depth=4, class_weight=class_weight)
 
     decision_tree = decision_tree.fit(X_train, y_train)
 
@@ -36,4 +85,7 @@ if __name__ == '__main__':
                                     filled=True, rounded=True,
                                     special_characters=False)
     graph = graphviz.Source(dot_data)
+    graph.save()
     graph.view()
+    print("\n\n\n\n")
+    interactive_questions(decision_tree, x_labels, y_labels)
